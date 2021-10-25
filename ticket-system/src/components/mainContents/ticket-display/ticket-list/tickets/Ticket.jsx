bb4e337a54ec0f11adaee6c686d1a7d9b7e80aa2
sars723 <sarasalomonn@gmail.com>
2021-10-25 14:28:05 +0200

    displaying message in TicketDetail component

--- ticket-system/src/components/mainContents/ticket-display/ticket-list/tickets/Ticket.jsx
+++ ticket-system/src/components/mainContents/ticket-display/ticket-list/tickets/Ticket.jsx
@@ -1,21 +1,55 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { withRouter } from "react-router-dom";
 import "./Ticket.css";
-const Ticket = ({ ticket }) => {
+/* import { useHistory } from "react-router"; */
+const Ticket = ({ ticket, history }) => {
+  const [user, setUser] = useState(null);
+  /* let history = useHistory(); */
+  useEffect(async () => {
+    try {
+      const response = await fetch(
+        "http://localhost:3004/users/" + ticket.sender,
+        {
+          headers: {
+            authorization: `Bearer ${window.localStorage.getItem("Token")}`,
+          },
+        }
+      );
+      if (response.ok) {
+        const fetchedUser = await response.json();
+        console.log(fetchedUser);
+        setUser(fetchedUser);
+      } else {
+        alert("sth wrong");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
   return (
     <div className="ticket row mb-5">
       {console.log("newTicket", ticket)}
       <div className="col-md-4">
-        <h5>{ticket.subject}</h5>
+        <h5
+          onClick={() =>
+            history.push({
+              pathname: "/ticketDetail",
+              state: { ticketD: ticket },
+            })
+          }
+        >
+          {ticket.subject}
+        </h5>
         <div className="user-category">
-          <p>user</p>
-          <p>category</p>
+          <p>{user && user.name}</p>
+          <p>{ticket.category}</p>
         </div>
       </div>
       <div className="col-md-8 d-flex justify-content-between align-items-center ">
-        <p>High</p>
+        <p>{ticket.priority}</p>
         <p>New</p>
         <p>11 hours ago</p>
-        <p>sara</p>
+        <p>{user && user.name}</p>
         <p>2 hours ago</p>
         <input type="checkbox" />
       </div>
@@ -23,4 +57,4 @@ const Ticket = ({ ticket }) => {
   );
 };
 
-export default Ticket;
+export default withRouter(Ticket) /* Ticket */;
